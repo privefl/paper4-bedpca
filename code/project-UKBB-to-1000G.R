06d661ae6ce03423cc47205e95b8d4f330533567
Florian Privé <florian.prive.21@gmail.com>
2019-10-16 11:00:26 +0200

    20K points until PC16

--- code/project-UKBB-to-1000G.R
+++ code/project-UKBB-to-1000G.R
@@ -20,24 +20,24 @@ round(shrinkage, 2)
 #  [1] 1.01 1.02 1.06 1.08 1.35 1.82 2.32 2.35 2.78 2.84 2.98 3.47 4.37
 # [14] 4.64 4.94 5.30 5.74 6.49 6.70 6.74
 
-ind <- sample(nrow(proj1), 10e3)
+ind <- sample(nrow(proj1), 20e3)
 
-plot_grid(plotlist = lapply(1:6, function(k) {
+plot_grid(plotlist = lapply(1:8, function(k) {
   k1 <- 2 * k - 1
   k2 <- 2 * k
   plot_grid(
     qplot(PC.ref[, k1], PC.ref[, k2], size = I(2)) +
       geom_point(aes(proj1[ind, k1], proj1[ind, k2]), color = "red") +
-      theme_bigstatsr(0.7) +
+      theme_bigstatsr(0.5) +
       labs(x = paste0("PC", k1), y = paste0("PC", k2)),
     qplot(PC.ref[, k1], PC.ref[, k2], size = I(2)) +
       geom_point(aes(proj2[ind, k1], proj2[ind, k2]), color = "blue") +
-      theme_bigstatsr(0.7) +
+      theme_bigstatsr(0.5) +
       labs(x = paste0("PC", k1), y = paste0("PC", k2)),
     scale = 0.95
   )
-}), nrow = 3)
+}), nrow = 4)
 
-# ggsave("figures/proj1000G-UKBB.pdf", width = 10.5, height = 6.5)
+# ggsave("figures/proj1000G-UKBB.png", width = 11, height = 7)
 
 
