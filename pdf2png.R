ed7455a747a4aef03dfe4b1be6073e83ebfe2150
Florian Privé <florian.prive.21@gmail.com>
2019-11-01 13:38:28 +0100

    proper SVGs

--- pdf2png.R
+++ pdf2png.R
@@ -3,6 +3,6 @@ library(magick)
 "figures/us.pdf" %>% {
   svg <- sub("\\.pdf$", ".svg", .)
   img <- image_read_pdf(.)
-  image_write(img, svg)
+  image_write(img, svg, format = "SVG")
   print(svg)
 }
