5aa67e0029831b125879e4f9a1afce260f3238e6
Florian Privé <florian.prive.21@gmail.com>
2019-12-13 15:23:45 +0100

    save

--- pdf2png.R
+++ pdf2png.R
@@ -6,3 +6,10 @@ library(magick)
   image_write(img, svg, format = "SVG")
   print(svg)
 }
+
+"figures/outliers-1000G.pdf" %>% {
+  svg <- sub("\\.pdf$", ".png", .)
+  img <- image_read_pdf(.)
+  image_write(img, svg, format = "PNG")
+  print(svg)
+}
